675cf712ad1aaaeffd4ac19658d939a6e3cf594c
zhanggx@fzswan.cn <zhangguangxito@sina.cn>
2020-09-15 16:35:55 +0800

    配置整合

--- telei-wms-api/src/main/java/com/telei/wms/project/api/configuration/CustomizeConfiguration.java
+++ telei-wms-api/src/main/java/com/telei/wms/project/api/configuration/CustomizeConfiguration.java
@@ -1,7 +1,14 @@
 package com.telei.wms.project.api.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.text.SimpleDateFormat;
 
 
 /**
@@ -16,4 +23,17 @@ public class CustomizeConfiguration {
         this.customizeProperties = customizeProperties;
     }
 
+    @Bean("mappingJackson2HttpMessageConverter")
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.class,ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        objectMapper.setDateFormat(smt);
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        return mappingJackson2HttpMessageConverter;
+    }
 }
